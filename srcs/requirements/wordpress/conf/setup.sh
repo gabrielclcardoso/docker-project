feaf939d418f3197333dff8e2e5eda01e3cb26d6
Gabriel Cardoso <gabrielclcardoso@gmail.com>
2023-06-06 11:37:20 -0300

    Feat: wordpress user setup implemented

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -2,16 +2,22 @@
 
 if [ ! -d "/www/wordpress" ]
 then
-	wget https://wordpress.org/latest.tar.gz
-	tar -xvf latest.tar.gz
-	rm latest.tar.gz
-	mv wordpress www/
+	wp core download --path=/www/wordpress/
+	if [ $? != 0 ]
+	then
+		rmdir /www/wordpress
+	fi
 	cd www/wordpress
 	mv wp-config-sample.php wp-config.php
 	sed -i "s/database_name_here/$DB_NAME/g" wp-config.php
 	sed -i "s/username_here/$MARIADB_USR/g" wp-config.php
 	sed -i "s/password_here/$MARIADB_PASSWD/g" wp-config.php
 	sed -i "s/localhost/$DB_HOST:3306/g" wp-config.php
+	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE \
+		--admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWD \
+		--admin_email=$EMAIL --skip-email
+	wp user create $WP_USER $WP_EMAIL --user_pass=$WP_USER_PASSWD
+
 fi
 
-exec php-fpm8 -F
+exec php-fpm7 -F
