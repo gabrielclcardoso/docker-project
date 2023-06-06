53fc098c6081787e2fa7d28daf3a87ea2f0b464d
Gabriel Cardoso <gabrielclcardoso@gmail.com>
2023-06-06 12:54:35 -0300

    Feat: moved ssl certs to .env file

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
-#Create ssl certificate
+#import ssl certificate
 mkdir -p /etc/ssl/private
-openssl req -x509 -nodes -days 1 -newkey rsa:2048 \
-	-keyout /etc/ssl/private/nginx-selfsigned.key \
-	-out /etc/ssl/certs/nginx-selfsigned.crt << END
-$COUNTRY
-$CITY
-$STATE
-$COMPANY
-$TEAM
-$DOMAIN_NAME
-$EMAIL
-END
+echo "-----BEGIN CERTIFICATE-----" > /etc/ssl/certs/nginx-selfsigned.crt
+echo $SELF_SIGNED_CRT >> /etc/ssl/certs/nginx-selfsigned.crt
+echo -n "-----END CERTIFICATE-----" >> /etc/ssl/certs/nginx-selfsigned.crt
+
+echo "-----BEGIN PRIVATE KEY-----" > /etc/ssl/private/nginx-selfsigned.key
+echo $SELF_SIGNED_KEY >> /etc/ssl/private/nginx-selfsigned.key
+echo -n "-----END PRIVATE KEY-----" >> /etc/ssl/private/nginx-selfsigned.key
 
 exec nginx
