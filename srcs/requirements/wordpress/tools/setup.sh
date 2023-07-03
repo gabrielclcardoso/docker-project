7d513d0888ec4c36781d56acdf378f524f764d68
gcorriea <gcorreia@supermeia.gcorreia.42.fr>
2023-07-03 15:16:48 -0300

    fix: fixed user creation on wordpress

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -18,7 +18,7 @@ then
 	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE \
 		--admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWD \
 		--admin_email=$WP_EMAIL --skip-email
-	wp user create $WP_USER $WP_EMAIL --user_pass=$WP_USER_PASSWD
+	wp user create $WP_USER $EMAIL --user_pass=$WP_USER_PASSWD
 fi
 
 exec php-fpm7 -F
